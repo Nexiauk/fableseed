1dac256615db132713aabb2d0d304916472f4410
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-12 11:53:16 +0100

    changed the test name for clarity and added docstrings

--- stories/tests.py
+++ stories/tests.py
@@ -2,8 +2,18 @@ from django.test import TestCase
 from django.urls import reverse
 
 # Create your tests here.
+
 class SimpleViewTest(TestCase):
-    def test_hello_world_stories_app(self):
+    """
+    A class of tests designed specifically to test each view as its created.
+    Tests that the view returns an HTTP 200 status code and contains the 
+    text 'Hello world" in the response
+    """
+    def test_hello_world_index_view(self):
+        """
+        Tests that the index view in the stories app returns an HTTP 200 response code.
+        Tests that it contains "Hellow world" in the response.
+        """
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Hello world")
